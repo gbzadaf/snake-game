5e3aa3d9c2a999dc0c45cebc6dc5ef92648b6f24
gbzadaf <fonsecagb23@gmail.com>
2026-05-21 18:39:12 -0300

    extra: black background and colorful snake

--- src/application/GamePanel.java
+++ src/application/GamePanel.java
@@ -53,13 +53,19 @@ public class GamePanel extends JPanel implements ActionListener {
     public void draw(Graphics g) {
 
         if (running) {
-        for (int i=0; i<SCREEN_HEIGHT/UNIT_SIZE; i++) {
+        /*for (int i=0; i<SCREEN_HEIGHT/UNIT_SIZE; i++) {
 
             g.drawLine(i*UNIT_SIZE, 0, i*UNIT_SIZE, SCREEN_HEIGHT);
             g.drawLine(0, i*UNIT_SIZE, SCREEN_WIDTH, i*UNIT_SIZE );
+
+
         }
+        
+         */
         g.setColor(Color.red);
         g.fillOval(appleX, appleY, UNIT_SIZE, UNIT_SIZE);
+
+
 
         for (int i = 0; i< bodyParts; i++) {
             if (i == 0) {
@@ -67,6 +73,7 @@ public class GamePanel extends JPanel implements ActionListener {
                 g.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
             } else {
                 g.setColor(new Color(45, 180, 0));
+                g.setColor(new Color(random.nextInt(255), random.nextInt(255), random.nextInt(255)));
                 g.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
             }
             g.setColor(Color.red);
